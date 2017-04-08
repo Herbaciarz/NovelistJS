3ea8f2016e03aa37348e159021c888a1af69ed0d
Dominik Zdziarski <kontakt@herbaciarz.com>
2017-04-08 20:33:25 +0200

    Index head

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const process = require('process');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const novelist = require('./components/novelist');
 
 const port = 3000;
 const serverUrl = 'localhost';
@@ -12,20 +12,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  let configFile = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-  
-  res.render('pages/index', {
-    header: {
-      'title': configFile['title'],
-      'description': configFile['description'],
-      'keywords': configFile['keywords'],
-      'robots': configFile['robots'],
-      'author': configFile['author'],
-      'favicon': configFile['favicon']
-    }
-  });
+  res.render('pages/index', { header: novelist.headConfig(), blog: novelist.blogConfig() });
 });
-
 
 app.listen(port, function(){
   console.log('Website is running on ' + serverUrl + ':' + port + ' port');
