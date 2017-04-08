1b5169918d8de3b0c834c92a297d91b2f5837991
Dominik Zdziarski <kontakt@herbaciarz.com>
2017-04-08 22:46:45 +0200

    Simplify, recolor, social icons

--- components/novelist.js
+++ components/novelist.js
@@ -3,25 +3,11 @@ const fs = require('fs');
 module.exports = {
   headConfig: function() {
     let configFile = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-    let header = {
-      'title': configFile.title,
-      'description': configFile.description,
-      'keywords': configFile.keywords,
-      'robots': configFile.robots,
-      'author': configFile.author,
-      'favicon': configFile.favicon
-    };
-    return header;
+    return configFile;
   },
   blogConfig: function() {
     let blogFile = JSON.parse(fs.readFileSync('./blog.json', 'utf8'));
-    let blog = {
-      'title': blogFile.title,
-      'description': blogFile.description,
-      'writter': blogFile.writter,
-      'logo': blogFile.logo
-    };
-    return blog;
+    return blogFile;
   }
 
 }
