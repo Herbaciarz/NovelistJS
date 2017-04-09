e670856d1bba21a2e67b03db1b3e32d36b7927ca
Dominik Zdziarski <kontakt@herbaciarz.com>
2017-04-09 21:34:54 +0200

    Retrieve all, single and add post

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+// packages and dependencies
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const novelist = require('./models/novelist');
 const assert = require('assert');
+const posts = require('./models/posts');
 
+// constants
 const port = 3000;
 const serverUrl = 'localhost';
 
+// config
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// routing
 app.get('/', function (req, res) {
     res.render('pages/index', {header: novelist.headConfig(), blog: novelist.blogConfig()});
 });
 
+// 404 error
+app.get('*', function (req, res) {
+    res.status(404).send('404 File not found');
+});
+
+// listening
 app.listen(port, function () {
     console.log('Website is running on ' + serverUrl + ':' + port + ' port');
 });
--- models/posts.js
+++ models/posts.js
@@ -5,13 +5,50 @@ const mongoUrl = 'mongodb://localhost:27017/test';
 
 module.exports = {
     getAllPosts: function () {
-
+        var postsData = [];
+        mongo.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            let cursor = db.collection('testc').find();
+            cursor.forEach(function (doc, err) {
+                assert.equal(null, err);
+                postsData.push(doc);
+            }, function () {
+                db.close();
+            });
+        });
+        return postsData;
     },
-    getSinglePost: function () {
-
+    getSinglePost: function (postId) {
+        var postData = [];
+        mongo.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            let cursor = db.collection('testc').find({postId: postId});
+            cursor.forEach(function (doc, err) {
+                assert.equal(null, err);
+                postData.push(doc);
+            }, function () {
+                db.close();
+            });
+        });
+        return postData;
     },
-    addPost: function () {
-        
+    addPost: function (date, author, title, content, tags) {
+        var postData = {
+            postID: 0,
+            date: date,
+            author: author,
+            title: title,
+            content: content,
+            tags: tags
+        };
+
+        mongo.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            db.collection('testc').insertOne(postData, function (err, result) {
+                assert.equal(null, err);
+                console.log('Item inserted!');
+            });
+        });
     },
     editPost: function () {
         
