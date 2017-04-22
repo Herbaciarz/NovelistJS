c57019350a0418ae2f6dab78b63b289bacd5003d
Dominik Zdziarski <kontakt@herbaciarz.com>
2017-04-23 00:13:44 +0200

    Fix favicon

--- models/novelist.js
+++ models/novelist.js
@@ -3,7 +3,6 @@ const Fs = require('fs');
 module.exports = {
   getConfig: function() {
       let configFile = JSON.parse(Fs.readFileSync('./config.json', 'utf8'));
-      console.log(configFile);
       return configFile;
   }
 }
